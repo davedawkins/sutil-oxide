d1651c406038c3a50253e1ef05998e540f083ecd
David Dawkins <david.dawkins@gmail.com>
2026-01-14 12:38:00 +0000

    refactor: getCallStack() helper

--- src/SutilOxide/loghelper.js
+++ src/SutilOxide/loghelper.js
@@ -7,3 +7,15 @@ export function trapConsoleLog( f ) {
         f(arguments);
     };
 }
+
+function getErrorObject(){
+    try { throw Error('') } catch(err) { return err; }
+}
+
+export function getCallStack() {
+    var err = getErrorObject();
+    var caller_line = err.stack.split("\n")[4];
+    var index = caller_line.indexOf("at ");
+    var clean = caller_line.slice(index+2, caller_line.length);
+    return err.stack.split("\n")
+}
